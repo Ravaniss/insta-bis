47723ee835b609d4e274e1f7bfb7f6d6fa52caf7
Ravaniss <senechal.max06@gmail.com>
2019-07-09 16:01:22 +0200

    package.json - Install dependencies

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('hello world ! :D');
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const mongoose = require("mongoose");
+const config_1 = require("./config");
+const user_1 = require("./routes/user");
+mongoose.connect(config_1.mongo_uri, { useNewUrlParser: true })
+    .then(() => {
+    const app = express();
+    const cors = (req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', '*');
+        res.header('Access-Control-Allow-Headers', '*');
+        next();
+    };
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(cors);
+    app.use('/user', user_1.router);
+    app.listen(config_1.port, console.log(`Server has started on http://localhost:${config_1.port}`));
+}).catch(err => console.error(`An error has occured ${err}`));
--- server.ts
+++ server.ts
@@ -1,1 +1,30 @@
-console.log('hello world ! :D')
+import express = require('express')
+import mongoose = require('mongoose')
+
+import { port, mongo_uri } from './config'
+import { router } from './routes/user'
+
+mongoose.connect(mongo_uri, { useNewUrlParser: true })
+  .then(() => {
+    const app: any = express()
+
+    const cors = (req: any, res: any, next: any) => {
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Methods', '*')
+      res.header('Access-Control-Allow-Headers', '*')
+      next()
+    }
+
+    app.use(express.urlencoded({extended: true}))
+    app.use(express.json())
+    app.use(cors)
+
+    app.use('/user', router)
+
+    app.listen(port, console.log(`Server has started on http://localhost:${port}`))
+  }).catch(err => console.error(`An error has occured ${err}`))
+
+
+
+
+
