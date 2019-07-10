019588c13e338289c55e3013f57ada648d4a7f61
Ravaniss <senechal.max06@gmail.com>
2019-07-10 16:44:46 +0200

    Footer - CSS

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -7,3 +7,18 @@
     <button>Profile</button>
   </footer>
 </template>
+
+<style lang="scss" scoped>
+  footer {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    position: fixed;
+    height: 40px;
+    background-color: #EEE;
+    border-top: 1px solid #CCC;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+</style>
