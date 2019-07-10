77b1d602312bcbedc145899e6fab8a79e691407d
Ravaniss <senechal.max06@gmail.com>
2019-07-10 14:05:07 +0200

    Hash class updated

--- back/scripts/Hash.ts
+++ back/scripts/Hash.ts
@@ -1,11 +1,11 @@
 import * as argon2 from 'argon2'
 
 export class Password {
-  static hash (password: string) {
+  public static hash (password: string) {
     return argon2.hash(password)
   }
 
-  static compare (hash: any, password: string) {
+  public static compare (hash: any, password: string) {
     return argon2.verify(hash, password)
   }
 }
