1bb7c08e1b8382c4903aebaf8457588d01d261e9
Ravaniss <senechal.max06@gmail.com>
2019-07-11 20:28:16 +0200

    Profile - Make profile more beauty

--- front/src/views/Profile.vue
+++ front/src/views/Profile.vue
@@ -22,9 +22,49 @@
   export default class Profile extends Vue {
     display_name: string = ''
     posts: any = []
+
+    getProfile () {
+      this.$http.post(this.$store.state.api + 'user/getprofile', {
+        auth_token: localStorage.getItem('jwt')
+      }).then(({ data }) => {
+        console.log(data)
+        this.display_name = data.details.display_name
+        this.posts = data.details.posts
+      })
+    }
+
+    mounted () {
+      this.getProfile()
+    }
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+  .profile {
+    display: flex;
+    flex-direction: column;
 
+
+    .profileHead {
+      background: #FFF;
+      padding: 15px;
+      box-sizing: border-box;
+    }
+
+    .posts {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-auto-rows: min-content;
+      grid-gap: 5px;
+
+      .post {
+        padding: 0;
+
+        .postImage {
+          width: 100%;
+          display: block;
+        }
+      }
+    }
+  }
 </style>
