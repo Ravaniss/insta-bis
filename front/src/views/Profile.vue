202f7bfd4c5cb5f9c5edeedf3ccfeed5860c3487
Ravaniss <senechal.max06@gmail.com>
2019-07-11 20:13:12 +0200

    Profile - Create profile view (not ended)

---
+++ front/src/views/Profile.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="view profile">
+    <section class="profileHead">
+      {{ display_name }}
+    </section>
+
+    <section class="posts">
+      <div
+        class="post"
+        v-for="post in posts"
+        :key="post._id">
+        <img :src="post.image" :alt="post.desc" class="postImage">
+      </div>
+    </section>
+  </main>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+
+  @Component
+  export default class Profile extends Vue {
+    display_name: string = ''
+    posts: any = []
+  }
+</script>
+
+<style lang="scss">
+
+</style>
