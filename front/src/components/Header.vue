efa70286aa94b2e323b16f4824db75e01af4c6b9
Ravaniss <senechal.max06@gmail.com>
2019-07-11 20:45:02 +0200

    Header - updated with icons and logout btn

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -3,11 +3,32 @@
     <h3 class="title">
       INSTA<span>CLONE</span>
     </h3>
+    <button class="logoutBtn" @click="logout()">
+      <i class="material-icons">power_off</i>
+    </button>
   </header>
 </template>
 
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+  import { mapActions } from 'vuex'
+  import store from '@/store'
+
+  @Component({
+    store,
+    methods: {
+      ...mapActions([
+        'logout'
+      ])
+    }
+  })
+  export default class Header extends Vue {}
+</script>
+
 <style lang="scss" scoped>
   header {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
     position: fixed;
     top: 0;
     left: 0;
@@ -25,10 +46,22 @@
       line-height: 40px;
       text-align: center;
       font-weight: 900;
+      grid-column: 2/3;
 
       span {
         font-weight: 300;
       }
+    }
+
+    .logoutBtn {
+      appearance: none;
+      border: none;
+      outline: none;
+      background: none;
+      margin: 0 15px 0 0;
+      padding: 0;
+      grid-column: 3/4;
+      justify-self: end;
     }
   }
 </style>
